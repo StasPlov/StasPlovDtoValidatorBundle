183846ac7a9290ce1b4b3559780a5a86c9659d1e
StasPlov <SaviouR.S@mail.ru>
2024-01-09 01:04:09 +0000

    init

---
+++ StasPlovDtoValidatorBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the StasPlovDtoValidatorBundle.
+ *
+ * (c) Stas Plov <SaviouR.S@mail.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StasPlov\DtoValidatorBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Stas Plov <SaviouR.S@mail.ru>
+ */
+class DtoValidatorBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new DependencyInjection\Compiler\CorsConfigurationProviderPass());
+    }
+}
